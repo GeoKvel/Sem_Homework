3eb8b30e74a0b964b404f558bfe4190023d81076
georgii <georg25@bk.ru>
2023-05-23 16:39:04 +0400

    Task 4 comp-ted + minor fixes on Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,16 +3,12 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+Console.Clear();
 System.Console.WriteLine("Введите число a ");
 int a = int.Parse(Console.ReadLine()!);
 System.Console.WriteLine("Введите число b ");
 int b = int.Parse(Console.ReadLine()!);
 int Max = a;
 if (b > Max)
-{
     Max = b;
-    System.Console.WriteLine($"Максимальное число = {Max}");
-    
-}
-else 
-    System.Console.WriteLine($"Максимальное число = {Max}");
+System.Console.WriteLine($"Максимальное число = {Max}");
---
+++ Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+System.Console.WriteLine("Введите число a ");
+int a = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите число b ");
+int b = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите число c ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+if (b > max)
+    max = b;
+if (c > max)
+    max = c;
+
+System.Console.WriteLine(max);
+
+
+
+
+// {
+//     max = b;
+//     else if (c > max)
+//         max = c;
+//     if (c>max)
+//         max = c;    
+// }
+// System.Console.WriteLine(max);
